0651a9da2c06fdc317cad7608939a90e121896ed
Ash Hellwig <ahellwig.dev@gmail.com>
2025-04-27 00:51:50 -0600

    add constants for robinhood interaction

--- marketdata.py
+++ marketdata.py
@@ -9,10 +9,14 @@ outputting the result to the specified data directory, defaulting to
 """
 
 from datetime import datetime
+import matplotlib.pyplot as plt
 
 from logger import setup_logger
-from load_cfg import DATA_OUTPUT_DIRECTORY
-# import robin_stocks.robinhood as rh
+from load_cfg import \
+    DATA_OUTPUT_DIRECTORY, \
+    ROBINHOOD_USERNAME, \
+    ROBINHOOD_PASSWORD
+import robin_stocks.robinhood as r
 
 
 def generate_output_filename() -> str:
@@ -69,3 +73,48 @@ if __name__ == "__main__":
     market_data = MarketData(output_directory=DATA_OUTPUT_DIRECTORY)
 
     output_dir = market_data.output_directory
+
+    login = r.login(
+        username=ROBINHOOD_USERNAME,
+        password=ROBINHOOD_PASSWORD,
+        by_sms=False
+    )
+
+    symbol = "AAPL"
+    symbol_name = r.get_name_by_symbol(symbol)
+    expiration_date = "2025-05-09"
+    strike = 205
+    option_type = "call"
+    interval = "hour"
+    span = "week"
+    bounds = "regular"
+    info = None
+
+    historical_data = r.get_option_historicals(
+        symbol,
+        expiration_date,
+        strike,
+        option_type,
+        interval,
+        span,
+        bounds,
+        info
+    )
+
+    dates = []
+    closing_prices = []
+    open_prices = []
+
+    for data_point in historical_data:
+        dates.append(data_point["begins_at"])
+        closing_prices.append(data_point["close_price"])
+        open_prices.append(data_point["open_price"])
+
+    x = [datetime.strptime(d, "%Y-%m-%dT%H:%M:%SZ") for d in dates]
+
+    plt.plot(x, closing_prices, "ro")
+    plt.plot(x, open_prices, "bo")
+    plt.title(f"Option price for {symbol_name} over time")
+    plt.xlabel("Dates")
+    plt.ylabel("Price")
+    plt.show()
